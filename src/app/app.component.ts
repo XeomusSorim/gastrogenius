1462ef23338df867d5dc594b46da7755e6b95d3f
EstebaNava <160206797+XeomusSorim@users.noreply.github.com>
2024-06-16 16:28:49 -0600

    commit 1
    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, LoginComponent, NavbarComponent],
+  template: `
+    <router-outlet></router-outlet>
+    <app-navbar></app-navbar>
+    <app-login></app-login>
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'gastrogenius';
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,23 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'g4str0g3nius',
+        appId: '1:969760392748:web:a29ae5e00949ee43447389',
+        storageBucket: 'g4str0g3nius.appspot.com',
+        apiKey: 'AIzaSyCgXJI45LFQXNhRaj-9BynVMcZ055CkPO0',
+        authDomain: 'g4str0g3nius.firebaseapp.com',
+        messagingSenderId: '969760392748',
+      })
+    ),
+    provideFirestore(() => getFirestore()),
+  ],
 };
